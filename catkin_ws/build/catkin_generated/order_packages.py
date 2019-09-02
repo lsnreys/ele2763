5dcbddf7cd9116fa33e5551ef4a5d3f0b93fc057
Luciana Reys <lsnreys@gmail.com>
2019-09-02 19:32:30 -0300

    First commit

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ELE/lreys/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/ELE/lreys/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/ELE/lreys/catkin_ws/devel;/opt/ros/kinetic" != "" else []
